2eb5f786d8898e1f21554182715a8fcad90dee82
yuvalqm <yuval@quantum-machines.co>
2023-09-12 20:36:29 +0300

    the bird now work as it should with gravity

--- flappy_bird/flappy.py
+++ flappy_bird/flappy.py
@@ -29,23 +29,23 @@ max_ray_age = 2  # s
 # the delay between spawning rays. I.e. spawns at most 1 ray in that duration
 ray_spawn_delay = 0.1  # s
 
-# Number of asteroids to be spawned
-N_asteroids = 2  # 1
+# Number of pillars to be spawned
+N_pillars = 2  # 1
 
-# radius of the asteroids
-R_asteroid = field_size * 0.075  # V
+# radius of the pillars
+R_pillar = field_size * 0.075  # V
 
-# speed with which the asteroids move
-v_asteroid = 0.2  # V/s
+# speed with which the pillars move
+v_pillar = 0.2  # V/s
 
-# acceleration the ship experiences then the play presses the forward button
-ship_acceleration = 0.5 * 2  # (2*)V/s^2
+# acceleration the bird experiences then the play presses the forward button
+bird_acceleration = 0.5 * 2  # (2*)V/s^2
 
-# Max speed of the ship
+# Max speed of the bird
 max_speed = 1  # V/s
 
-# the rotational speed of the ship
-ship_rotation_speed = 2.0  # 2*pi/s
+# the rotational speed of the bird
+bird_rotation_speed = 2.0  # 2*pi/s
 
 # the amount of time the game is advanced every tick
 # as the timing is not implemented yet, this value sets the amount of time that passes for each tick.
@@ -66,6 +66,13 @@ wait_time = 1e7 / 2  # ns
 
 # the amplitude used to probe the controller.
 input_probe_voltage = .5  # V
+
+# Define additional game parameters
+gravity = 0.1  # Adjust gravity to control the bird's fall speed
+flap_force = -0.1  # Adjust flap_force to control the bird's jump height
+pillar_speed = 0.1  # Adjust pillar_speed to control the pillar's horizontal speed
+pillar_interval = 2.0  # Adjust pillar_interval to control the time between pillar spawns
+bird_y_max = field_size - 0.1  # Define the maximum height for the bird
 
 # %%
 
@@ -104,9 +111,8 @@ configuration = {
                 },
             },
             'operations': {
-                "ship": "ship",
-                "ray": "ray",
-                "asteroid": "asteroid",
+                "bird": "bird",
+                "pillar": "pillar",
                 "border": "border",
             },
         },
@@ -141,7 +147,7 @@ configuration = {
             'length': sprite_length,
             'waveforms': {k: f"{n}_{l}" for k, l in zip(["I", "Q"], ["x", "y"])},
         }
-            for n in ["bird", "tube", "border"]},
+            for n in ["bird", "pillar", "border"]},
         "measure_user_input": {
             "operation": "measurement",
             'length': user_input_pulse_length,
@@ -159,11 +165,11 @@ configuration = {
     'waveforms': {
         **{
             f"bird_{a}": {'type': 'arbitrary', 'samples': v}
-            for a, v in zip(["x", "y"], get_ship_pulse(sprite_length) * field_size * 0.1)
+            for a, v in zip(["x", "y"], get_bird_pulse(sprite_length) * field_size * 0.1)
         },
         **{
-            f"tube_{a}": {'type': 'arbitrary', 'samples': v}
-            for a, v in zip(["x", "y"], get_bird_pulse(sprite_length) * R_asteroid*2)
+            f"pillar_{a}": {'type': 'arbitrary', 'samples': v}
+            for a, v in zip(["x", "y"], get_pillar_pulse(sprite_length) * R_pillar*2)
         },
         **{
             f"border_{a}": {'type': 'arbitrary', 'samples': v}
@@ -218,11 +224,20 @@ def draw_by_name(name, x, y, a):
     align()
 
 
-def draw_ship(x, y, a):
+def draw_bird(x, y, a):
     move_cursor(x, y)
-    play('ship' * get_rot_amp(a), 'screen')
+    play('bird' * get_rot_amp(a), 'screen')
     align()
 
+def draw_bird(x, y, a):
+    move_cursor(x, y)
+    play('bird' * get_rot_amp(a), 'screen')
+    align()
+
+def draw_pillar(x, y, a):
+    move_cursor(x, y)
+    play('bird' * get_rot_amp(a), 'screen')
+    align()
 
 def draw_ray(x, y, a):
     move_cursor(x, y)
@@ -230,9 +245,9 @@ def draw_ray(x, y, a):
     align()
 
 
-def draw_asteroid(x, y, a):
+def draw_pillar(x, y, a):
     move_cursor(x, y)
-    play('asteroid' * get_rot_amp(a), 'screen')
+    play('pillar' * get_rot_amp(a), 'screen')
     align()
 
 
@@ -248,12 +263,12 @@ def get_distance(ax, ay, bx, by):
     return distance
 
 
-def ray_hit(ray_x, ray_y, asteroid_x, asteroid_y):
+def ray_hit(ray_x, ray_y, pillar_x, pillar_y):
     hit = declare(bool, False)
 
-    distance = get_distance(ray_x, ray_y, asteroid_x, asteroid_y)
+    distance = get_distance(ray_x, ray_y, pillar_x, pillar_y)
 
-    with if_(distance < R_asteroid):
+    with if_(distance < R_pillar):
         assign(hit, True)
 
     return hit
@@ -317,26 +332,28 @@ rng = np.random.default_rng(seed=1234)
 # %%
 
 with program() as game:
-    ship_a = declare(fixed, 0)
-    ship_x = declare(fixed, 0)
-    ship_y = declare(fixed, 0)
-    ship_vx = declare(fixed, 0)
-    ship_vy = declare(fixed, 0)
+    bird_a = declare(fixed, 0)
+    bird_x = declare(fixed, 0)
+    bird_y = declare(fixed, 0)
+    bird_vx = declare(fixed, 0)
+    bird_vy = declare(fixed, 0)
 
-    rays_active = declare(bool, value=[False] + [False] * (N_rays - 1))
-    rays_age = declare(fixed, value=[max_ray_age] + [0] * (N_rays - 1))
-    rays_x = declare(fixed, value=[0] * N_rays)
-    rays_y = declare(fixed, value=[0] * N_rays)
-    rays_a = declare(fixed, value=[0] * N_rays)
+    pillars_active = declare(bool, value=[True] * N_pillars)
+    pillars_x = declare(fixed, value=rng.uniform(-field_size, field_size, N_pillars))
+    pillars_y = declare(fixed, value=rng.uniform(0, 0, N_pillars))
+    pillars_a = declare(fixed, value=rng.uniform(-.5, .5, N_pillars))
 
-    asteroids_active = declare(bool, value=[True] * N_asteroids)
-    asteroids_x = declare(fixed, value=rng.uniform(-field_size, field_size, N_asteroids))
-    asteroids_y = declare(fixed, value=rng.uniform(-field_size, field_size, N_asteroids))
-    asteroids_a = declare(fixed, value=rng.uniform(-.5, .5, N_asteroids))
+    # Add variables to keep track of game state
+    bird_flap = declare(int, 0)  # Variable to track if the player flaps
+    pillar_x = declare(fixed, field_size)  # Initial horizontal position of the pillars
+    pillar_y = declare(fixed, 0)  # Vertical position of the pillars
+    pillar_gap = 0.2  # Adjust pillar_gap to control the gap between pillars
+    score = declare(int, 0)  # Player's score
+    game_over = declare(bool, False)  # Flag to indicate game over
 
     t = declare(fixed, 0)
     t_prim = declare(fixed, 0)
-    t_last_ray_spawn = declare(fixed, -1.1)
+    t_last_pillar_spawn = declare(fixed, -1.1)
     dt = declare(fixed, 0)
     i = declare(int, 0)
     j = declare(int, 0)
@@ -367,6 +384,8 @@ with program() as game:
         assign(move, 0)  # The user input
         assign(act, 0)  # The user input
 
+
+
         '''
         The inputs
         w - forward
@@ -379,11 +398,11 @@ with program() as game:
 
         get_inputs(move, act)
         with if_(move == 1):
-            assign(ui_forward, 1)
-        with elif_(move == 3):
-            assign(ui_phi, -1)
-        with elif_(move == 4):
-            assign(ui_phi, 1)
+            assign(bird_flap, 1)
+        # with elif_(move == 3):
+        #     assign(ui_phi, -1)
+        # with elif_(move == 4):
+        #     assign(ui_phi, 1)
 
         with if_(act == 5):
             assign(ui_fire, True)
@@ -391,86 +410,69 @@ with program() as game:
             assign(cont, False)
 
 
-        # move ship
-        # update the rotation
-        assign(ship_a, ship_a + ui_phi * ship_rotation_speed * dt)
-        clip_angle(ship_a)
+        # move bird
 
-        # spawn rays
-        with if_(ui_fire):
-            with if_(ray_spawn_delay < t - t_last_ray_spawn):
-                assign(i, Math.argmin(rays_age))
-                assign(rays_active[i], True)
-                assign(rays_age[i], max_ray_age)
-                assign(rays_x[i], ship_x)
-                assign(rays_y[i], ship_y)
-                assign(rays_a[i], ship_a)
-                assign(t_last_ray_spawn, t)
 
         # # update the velocity and position
-        assign(ship_x, ship_x + ship_vx * dt)
-        assign(ship_y, ship_y + ship_vy * dt)
-        assign(ship_vx, ship_vx + Math.cos2pi(ship_a) * ui_forward * ship_acceleration * dt)
-        assign(ship_vy, ship_vy + Math.sin2pi(ship_a) * ui_forward * ship_acceleration * dt)
-        clip_velocity(ship_vy)
-        clip_velocity(ship_vx)
+        # assign(bird_x, bird_x + bird_vx * dt)
+        # assign(bird_y, bird_y + bird_vy * dt)
+        # assign(bird_vx, bird_vx + Math.cos2pi(bird_a) * ui_forward * bird_acceleration * dt)
+        # assign(bird_vy, bird_vy + Math.sin2pi(bird_a) * ui_forward * bird_acceleration * dt)
+        # clip_velocity(bird_vy)
+        # clip_velocity(bird_vx)
+        assign(bird_vx, bird_vx - gravity * dt)
+        assign(bird_x, bird_x + bird_vx * dt)
 
-        # process hits
-        with for_(i, 0, i < N_rays, i + 1):
-            with for_(j, 0, j < N_asteroids, j + 1):
-                with if_(rays_active[i] & asteroids_active[j]):
-                    # with if_(ray_hit(rays_x[i], rays_y[i], asteroids_x[j], asteroids_y[j])):
-                    with if_((get_distance(rays_x[i], rays_y[i], asteroids_x[j], asteroids_y[j]) < R_asteroid)):
-                        assign(rays_active[i], False)
-                        assign(rays_age[i], -1)
-                        assign(asteroids_active[j], False)
+        # Handle bird flapping
+        with if_(bird_flap == 1):
+            assign(bird_vx, -flap_force)
+            assign(bird_flap, 0)
+
+        # Spawn pillars
+        with if_(t - t_last_pillar_spawn >= pillar_interval):
+            assign(t_last_pillar_spawn, t)
+            assign(pillar_y, rng.uniform(-field_size + pillar_gap, field_size - pillar_gap))
+
+        # Move pillars
+        assign(pillar_x, pillar_x - pillar_speed * dt)
+
+        # Check for collisions
+        # with if_(bird_y > bird_y_max or bird_y < -bird_y_max):
+        #     assign(game_over, True)
+        #
+        # with if_(bird_x < pillar_x + R_pillar & bird_x > pillar_x - R_pillar):
+        #     with if_(bird_y > pillar_y + pillar_gap / 2 or bird_y < pillar_y - pillar_gap / 2):
+        #         assign(game_over, True)
+
+        # Update score
+        with if_(bird_x > pillar_x):
+            assign(score, score + 1)
 
         # process crashes
-        with for_(j, 0, j < N_asteroids, j + 1):
-            with if_(asteroids_active[j]):
-                # with if_(ray_hit(rays_x[i], rays_y[i], asteroids_x[j], asteroids_y[j])):
-                with if_((get_distance(ship_x, ship_y, asteroids_x[j], asteroids_y[j]) < R_asteroid)):
+        with for_(j, 0, j < N_pillars, j + 1):
+            with if_(pillars_active[j]):
+                # with if_(ray_hit(rays_x[i], rays_y[i], pillars_x[j], pillars_y[j])):
+                with if_((get_distance(bird_x, bird_y, pillars_x[j], pillars_y[j]) < R_pillar)):
                     assign(crashed, True)
 
-        # move rays
-        with for_(i, 0, i < N_rays, i + 1):
-            with if_(rays_active[i]):
-                # check age
-                with if_(rays_age[i] > 0):  # the ray is still alive
-                    assign(rays_age[i], rays_age[i] - dt)
-                    # update position
-                    assign(rays_x[i], rays_x[i] + Math.cos2pi(rays_a[i]) * v_ray * dt)
-                    assign(rays_y[i], rays_y[i] + Math.sin2pi(rays_a[i]) * v_ray * dt)
-                with else_():
-                    assign(rays_active[i], False)
-
-        # move asteroids
-        with for_(j, 0, j < N_asteroids, j + 1):
-            with if_(asteroids_active[j]):
-                assign(asteroids_x[j], asteroids_x[j] + Math.cos2pi(asteroids_a[j]) * v_asteroid * dt)
-                assign(asteroids_y[j], asteroids_y[j] + Math.sin2pi(asteroids_a[j]) * v_asteroid * dt)
+        # move pillars
+        with for_(j, 0, j < N_pillars, j + 1):
+            with if_(pillars_active[j]):
+                assign(pillars_x[j], pillars_x[j] + Math.cos2pi(pillars_a[j]) * v_pillar * dt)
+                assign(pillars_y[j], pillars_y[j] + Math.sin2pi(pillars_a[j]) * v_pillar * dt)
 
         # process border collisions
-        process_border_collisions(ship_x, ship_y)
-        with for_(i, 0, i < N_rays, i + 1):
-            with if_(rays_active[i]):
-                process_border_collisions(rays_x[i], rays_y[i])
-        with for_(i, 0, i < N_asteroids, i + 1):
-            with if_(asteroids_active[i]):
-                process_border_collisions(asteroids_x[i], asteroids_y[i])
+        process_border_collisions(bird_x, bird_y)
+        with for_(i, 0, i < N_pillars, i + 1):
+            with if_(pillars_active[i]):
+                process_border_collisions(pillars_x[i], pillars_y[i])
 
         # draw graphics
         play("marker_pulse", "draw_marker_element")
-        with if_(crashed):
-            draw_asteroid(ship_x, ship_y, ship_a)
-        with else_():
-            draw_ship(ship_x, ship_y, ship_a)
-        with for_(i, 0, i < N_rays, i + 1):
-            with if_(rays_active[i]):
-                draw_ray(rays_x[i], rays_y[i], rays_a[i])
-        with for_(i, 0, i < N_asteroids, i + 1):
-            with if_(asteroids_active[i]):
-                draw_asteroid(asteroids_x[i], asteroids_y[i], asteroids_a[i])
+        draw_bird(bird_x, bird_y, bird_a)
+        with for_(i, 0, i < N_pillars, i + 1):
+            with if_(pillars_active[i]):
+                draw_pillar(pillars_x[i], pillars_y[i], pillars_a[i])
         draw_border()
 
         # wait until everything is drawn
